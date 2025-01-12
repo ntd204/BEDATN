87c4d019dc825196e93264bedaa9fa88060acdf6
Trong Dao <trongdao20402@gmail.com>
2025-01-12 13:28:54 +0700

    Test bỏ check cors

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 require("dotenv").config();
 import cors from "cors";
 import initRoutes from "./src/routes/index.js";
-import connectDatabase from "./src/config/connectDatabase.js";
+// import connectDatabase from "./src/config/connectDatabase.js";
 
 // dotenv.config();
 process.env.TZ = "Asia/Ho_Chi_Minh";
@@ -11,29 +11,29 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({ message: "Ok" });
 });
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    methods: ["POST", "GET", "PUT", "DELETE"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL,
+//     methods: ["POST", "GET", "PUT", "DELETE"],
+//   })
+// );
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 initRoutes(app);
-connectDatabase();
+// connectDatabase();
 
 const port = process.env.PORT || 8888;
 const listener = app.listen(port, () => {
   console.log(`Server is running on the port ${listener.address().port}`);
 });
-app.get("/api/connectDatabase", async (req, res) => {
-  try {
-    await connectDatabase();
-    res.status(200).json({ message: "Database connected successfully!" });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Database connection failed!", details: error.message });
-  }
-});
+// app.get("/api/connectDatabase", async (req, res) => {
+//   try {
+//     await connectDatabase();
+//     res.status(200).json({ message: "Database connected successfully!" });
+//   } catch (error) {
+//     res
+//       .status(500)
+//       .json({ error: "Database connection failed!", details: error.message });
+//   }
+// });
