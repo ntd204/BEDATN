02abea60e5888fdb5cab1daa641e28754b6377d8
Trong Dao <trongdao20402@gmail.com>
2025-01-12 14:09:57 +0700

    update package json

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({ message: "Ok" });
 });
-// app.use(
-//   cors({
-//     origin: process.env.CLIENT_URL,
-//     methods: ["POST", "GET", "PUT", "DELETE"],
-//   })
-// );
+app.use(
+  cors({
+    // origin: process.env.CLIENT_URL,
+    methods: ["POST", "GET", "PUT", "DELETE"],
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
